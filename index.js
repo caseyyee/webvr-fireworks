2d4fbbacb517aefc05a449fbdbc94f27d314d20d
caseyyee <caseyyee.ca@gmail.com>
2017-01-04 17:36:19 -0800

    basic cleanup

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 
 if ( WEBVR.isAvailable() === false ) {
-
   document.body.appendChild( WEBVR.getMessage() );
-
 }
-
-//
 
 var clock = new THREE.Clock();
 
@@ -15,20 +11,23 @@ var effect, controls;
 var controller1, controller2;
 
 var fireworks = [];
-var firework, flame, flameLight;
+var firework;
 
 var particleSystem;
 var particles;
-var particleCount = 100
-var flameLightIntensity = 2;
-var fireworksLight;
+var particleCount = 100;             // lighter flame particles
+
+var lighterFlame;
+var lighterFlameLight;
+var lighterLightIntensity = 2;      // intensity of lighter flame light.
 var lighterOn = false;
-var lighterTheme = [ // flame colors
-    0xFF218C,
-    0xFFD800,
-    0x21B1FF,
-    0x21B1FF
-  ];
+
+var colorTheme = [
+  0xFF218C,
+  0xFFD800,
+  0x21B1FF,
+  0x21B1FF
+];
 
 init();
 
@@ -61,71 +60,82 @@ function init() {
   fillLight.position.set(600, 100, 18);
   scene.add(fillLight);
 
-  var keyLight = new THREE.PointLight(0xFC30B7, 0.5, 30);
-  keyLight.position.set(0, 1, 0);
-  scene.add(keyLight);
+  var areaLight = new THREE.PointLight(0xFC30B7, 0.5, 30);
+  areaLight.position.set(0, 1, 0);
+  scene.add(areaLight);
 
-  // floor - will remove
+  // ground pattern
   var ground = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100, 30, 30 ), new THREE.MeshBasicMaterial( {wireframe: true, color: 0xffff00} ) );
   ground.rotation.x = Math.PI/2;
-  ground.position.y = 0.01;
   ground.position.z = -1
   scene.add( ground );
 
   // ground
   loadModel('models/ground2.json').then(function(obj) {
+    obj.position.y = -0.01;
     scene.add(obj);
   });
 
   // sky
-  var material = new THREE.MeshBasicMaterial({
-    side: THREE.BackSide,
-    depthWrite: false,
-    depthTest: false,
-    fog: false,
-    map: new THREE.TextureLoader().load('img/bg-3.png')
-  });
-
-  sky = new THREE.Mesh(new THREE.SphereGeometry(1000, 64, 32), material);
+  var sky = new THREE.Mesh(
+    new THREE.SphereGeometry(1000, 64, 32),
+    new THREE.MeshBasicMaterial({
+      side: THREE.BackSide,
+      depthWrite: false,
+      depthTest: false,
+      fog: false,
+      map: new THREE.TextureLoader().load('img/sky.png')
+    }));
   scene.add(sky);
 
+  // renderer
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.sortObjects = false;
   container.appendChild( renderer.domElement );
 
+  // VR controls
   controls = new THREE.VRControls( camera );
   controls.standing = true;
 
-  // controllers
+  // VIVE Controllers
+  // Controller used for Lighter
   controller1 = new THREE.ViveController( 0 );
   controller1.standingMatrix = controls.getStandingMatrix();
+
   controller1.addEventListener( 'triggerdown', function() {
     lighterOn = true;
-    if (flameLight.intensity === 0) {
-      flameLight.intensity = flameLightIntensity;
+    if (lighterFlameLight.intensity === 0) {
+      lighterFlameLight.intensity = lighterLightIntensity;
     }
-  } );
+  });
+
   controller1.addEventListener( 'triggerup', function() {
     lighterOn = false;
-    if (flameLight.intensity) {
-      flameLight.intensity = 0;
+    if (lighterFlameLight.intensity) {
+      lighterFlameLight.intensity = 0;
     }
-  } );
+  });
+
   scene.add( controller1 );
 
+
+  // Controller used for fireworks placement
   controller2 = new THREE.ViveController( 1 );
   controller2.standingMatrix = controls.getStandingMatrix();
+
   var holdingFirework = false;
+
   controller2.addEventListener('triggerdown', function() {
     if (!holdingFirework) {
-      var firework = new Firework(50, 10);
+      var firework = new Firework(50, 10, colorTheme);
       controller2.add(firework);
       fireworks.push(firework);
       holdingFirework = firework;
     }
-  })
+  });
+
   controller2.addEventListener('triggerup', function() {
     if (holdingFirework) {
       if (holdingFirework.parent) {
@@ -135,7 +145,6 @@ function init() {
         holdingFirework.position.set(world.x, world.y, world.z);
         controller2.updateMatrixWorld();
         holdingFirework.setRotationFromMatrix(controller2.matrixWorld);
-        //holdingFirework.rotation.set(controller2.rotation.x, controller2.rotation.y, controller2.rotation.z);
         scene.add(holdingFirework);
 
         // remove firework if not placed within hit zone?
@@ -143,13 +152,15 @@ function init() {
       }
       holdingFirework = false;
     }
-  })
+  });
+
   scene.add( controller2 );
 
+
+  // load controller model
   var loader = new THREE.OBJLoader();
   loader.setPath( 'models/obj/vive-controller/' );
   loader.load( 'vr_controller_vive_1_5.obj', function ( object ) {
-
     var loader = new THREE.TextureLoader();
     loader.setPath( 'models/obj/vive-controller/' );
 
@@ -157,91 +168,81 @@ function init() {
     controller.material.map = loader.load( 'onepointfive_texture.png' );
     controller.material.specularMap = loader.load( 'onepointfive_spec.png' );
 
-    // controller1.add( object.clone() );
+    // add only to firework hand.
     controller2.add( object.clone() );
+  });
 
-  } );
-
+  // render through VR effect
   effect = new THREE.VREffect( renderer );
 
   if ( WEBVR.isAvailable() === true ) {
-
     document.body.appendChild( WEBVR.getButton( effect ) );
-
   }
 
+  // invisible mesh for hit-testing
+  lighterFlame = new THREE.Mesh(
+    new THREE.SphereGeometry( 0.05, 5, 5),
+    new THREE.MeshLambertMaterial({ visible: false, color: 0xff8800}));
+  lighterFlame.position.z = -0.3;
 
-  // lighter
-  var colors = [];
+  lighterFlameLight = new THREE.PointLight(0xFD7BDB, lighterLightIntensity, 0.5);
+  lighterFlame.add(lighterFlameLight);
 
-  flame = new THREE.Mesh( new THREE.SphereGeometry( 0.05, 5, 5), new THREE.MeshLambertMaterial({ visible: false, color: 0xff8800}));
-  flame.position.z = -0.3;
-
-  flameLight = new THREE.PointLight(0xFD7BDB, flameLightIntensity, 0.5);
-  flame.add(flameLight);
-
-  loadModel('models/lighter.json').then(function(obj) {
-    obj.add(flame);
-    obj.position.z = 0.1;
-    obj.position.y = -0.01;
-    controller1.add(obj);
+  // loads lighter model, adds lighter flame.
+  loadModel('models/lighter.json').then(function(model) {
+    model.add(lighterFlame);
+    model.position.z = 0.1;
+    model.position.y = -0.01;
+    controller1.add(model);
   })
 
-  // lighter particles
+  // flame particles
   particles = new THREE.Geometry();
-  var pMaterial = new THREE.PointsMaterial({
-    size: 0.01,
-    vertexColors: THREE.VertexColors,
-    blending: THREE.AdditiveBlending,
-    transparent: true
-  });
+
+  var colors = [];
 
   for (var p = 0; p < particleCount; p++) {
     var pX = randomNumber(-0.01, 0.01),
         pY = 0,
         pZ = randomNumber(-0.01, 0.01),
         particle = new THREE.Vector3(pX, pY, pZ);
-    colors[p] = new THREE.Color(lighterTheme[getRandomInt(0, lighterTheme.length-1)]);
+    colors[p] = new THREE.Color(colorTheme[getRandomInt(0, colorTheme.length-1)]);
     particles.vertices.push(particle);
   }
-
   particles.colors = colors;
-
-  // create the particle system
-  particleSystem = new THREE.Points(particles,pMaterial);
+  particleSystem = new THREE.Points(particles,
+    new THREE.PointsMaterial({
+      size: 0.01,
+      vertexColors: THREE.VertexColors,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    }));
   scene.add(particleSystem);
 
   window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   effect.setSize( window.innerWidth, window.innerHeight );
-
 }
 
-//
-
 function animate() {
-
   effect.requestAnimationFrame( animate );
-  fireworks.forEach(function(firework){
+
+  fireworks.forEach(function(firework) {
     firework.update();
   });
 
   if (lighterOn) {
     // detect collision
-    fireworks.forEach(function(firework){
+    fireworks.forEach(function(firework) {
       if (firework.fuse) {
         var shellBB = new THREE.Box3().setFromObject(firework.fuse);
-        var flameBB = new THREE.Box3().setFromObject(flame);
-
+        var flameBB = new THREE.Box3().setFromObject(lighterFlame);
         var collision = shellBB.intersectsBox(flameBB);
-
         if (collision) {
           firework.light();
         }
@@ -262,18 +263,15 @@ function animate() {
       particle.y += 0.005;
     }
     particleSystem.geometry.verticesNeedUpdate = true;
-    particleSystem.position.setFromMatrixPosition(flame.matrixWorld);
+    particleSystem.position.setFromMatrixPosition(lighterFlame.matrixWorld);
   } else {
     particleSystem.material.visible = false;
   }
 
-
   render();
-
 }
 
 function render() {
-
   var delta = clock.getDelta() * 60;
 
   controller1.update();
@@ -282,5 +280,4 @@ function render() {
   controls.update();
 
   effect.render( scene, camera );
-
 }
